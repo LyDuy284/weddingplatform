2fcd5f1b6973250b894cc5b4b2d7afec8fc9ff38
LyDuy284 <lyhieuduy9190@gmail.com>
2024-08-23 22:51:35 +0700

    fix payment

--- src/main/java/com/fu/weddingplatform/constant/payment/vnpay/VNPayConstant.java
+++ src/main/java/com/fu/weddingplatform/constant/payment/vnpay/VNPayConstant.java
@@ -3,7 +3,7 @@ package com.fu.weddingplatform.constant.payment.vnpay;
 public final class VNPayConstant {
     public final static String VNP_PAY_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html?";
     public final static String VNP_RETURN_URL = "http://localhost:8080/payment/call-back";
-    public final static String VNP_RETURN_URL_SERVER = "https://the-day-eqh7h5gwadbga9fe.eastus-01.azurewebsites.net/payment/call-back";
+    public final static String VNP_RETURN_URL_SERVER = "https://thedaywedding-hkaybdgafndhecbn.southeastasia-01.azurewebsites.net/payment/call-back";
     public final static String VNP_REDIRECT_CLIENT = "http://localhost:3000/booking-history";
     public final static String VNP_RETURN_CLIENT_URL = "http://localhost:5173/payment?isSuccess=%s&amount=%s";
     public final static String VNP_TMN_CODE = "PODT5GPF";
